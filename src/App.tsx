5f77e1aeaebd9220558f06b43057abefba96bbed
jc <jcheese@treasure.lol>
2022-11-01 19:33:59 +0900

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import Smol from "./assets/smol.png";
-import React, { MouseEvent, useRef } from "react";
+import { MouseEvent } from "react";
 import { useSpring, animated as a, to } from "@react-spring/web";
 
 export default function App() {
   return (
     <div className="App">
-      <Holo
-        imgSrc="https://images.pokemontcg.io/swsh1/190_hires.png"
-        textureSrc="https://res.cloudinary.com/simey/image/upload/Dev/PokemonCards/illusion.webp"
-        alt="Pokemon"
-        width="300px"
-      />
+      <Holo alt="Smol" perspective={false} />
     </div>
   );
 }
@@ -26,19 +21,7 @@ const springR = {
 
 const round = (num: number, fix = 3) => parseFloat(num.toFixed(fix));
 
-const Holo = ({
-  imgSrc,
-  alt,
-  textureSrc,
-  width,
-}: {
-  imgSrc: string;
-  alt: string;
-  textureSrc: string;
-  width: string;
-}) => {
-  const ref = useRef(null);
-
+const Holo = ({ alt, perspective }: { alt: string; perspective: boolean }) => {
   const [rotate, rotateApi] = useSpring(
     () => ({
       x: 0,
@@ -66,8 +49,6 @@ const Holo = ({
     [springR]
   );
 
-  // const [scale, scaleApi] = useSpring(() => ({ scale: 1, config: springD }));
-
   const handleMouse = (
     e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>
   ) => {
@@ -76,8 +57,8 @@ const Holo = ({
     const rect = element.getBoundingClientRect();
 
     const absolute = {
-      x: e.clientX - rect.left, // get mouse position from left
-      y: e.clientY - rect.top, // get mouse position from right
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
     };
 
     const percent = {
@@ -89,7 +70,7 @@ const Holo = ({
       y: percent.y - 50,
     };
 
-    backgroundApi.set({
+    backgroundApi.start({
       x: round(50 + percent.x / 4 - 12.5),
       y: round(50 + percent.y / 3 - 16.67),
     });
@@ -97,10 +78,10 @@ const Holo = ({
     rotateApi.start({
       x: round(-(center.x / 3.5)),
       y: round(center.y / 2),
-      s: 1.1,
+      s: 1.03,
     });
 
-    glareApi.set({
+    glareApi.start({
       x: percent.x,
       y: percent.y,
       o: 1,
@@ -114,43 +95,74 @@ const Holo = ({
       s: 1,
     });
 
-    glareApi.set({ x: 50, y: 50, o: 0 });
-    backgroundApi.set({ x: 50, y: 50 });
+    glareApi.start({ x: 50, y: 50, o: 0 });
+    backgroundApi.start({ x: 50, y: 50 });
   };
 
+  const filter = to([glare.x, glare.y], (x, y) => {
+    const hyp =
+      (Math.sqrt((y - 50) * (y - 50) + (x - 50) * (x - 50)) / 50) * 0.3 + 0.5;
+    return `brightness(${hyp}) contrast(2) saturate(1.5)`;
+  });
+
   return (
-    <a.div
-      className="wrapper"
-      style={
-        {
-          "--posx": to(background.x, (value) => `${value}%`),
-          "--posy": to(background.y, (value) => `${value}%`),
-          "--hyp": to(
-            [glare.x, glare.y],
-            (x, y) => Math.sqrt((y - 50) * (y - 50) + (x - 50) * (x - 50)) / 50
-          ),
-          "--o": to(glare.o, (value) => value),
-          "--mx": to(glare.x, (value) => `${value}%`),
-          "--my": to(glare.y, (value) => `${value}%`),
-        } as React.CSSProperties
-      }
-    >
+    <a.div className="wrapper">
       <div className="card">
         <a.div
           className="cardFront"
-          ref={ref}
           onMouseMove={handleMouse}
           onMouseOut={handleMouseOut}
           style={{
-            transform: to(
-              [rotate.x, rotate.y, rotate.s],
-              (x, y, s) =>
-                `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+            transform: to([rotate.x, rotate.y, rotate.s], (x, y, s) =>
+              perspective
+                ? `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+                : `scale(${s})`
             ),
           }}
         >
           <img src={Smol} alt={alt} />
-          <div className="cardShine"></div>
+          <a.div
+            style={{
+              opacity: glare.o.to((o) => o),
+              backgroundPosition: to(
+                [background.x, background.y],
+                (x, y) => `center, 0% ${y}%, ${x}% ${y}%,
+              ${x}% ${y}%`
+              ),
+              backgroundImage: to(
+                [glare.x, glare.y],
+                (x, y) => `url("/pattern.png"),
+            repeating-linear-gradient(
+              0deg,
+              rgb(255, 119, 115) calc(var(--space) * 1),
+              rgba(255, 237, 95, 1) calc(var(--space) * 2),
+              rgba(168, 255, 95, 1) calc(var(--space) * 3),
+              rgba(131, 255, 247, 1) calc(var(--space) * 4),
+              rgba(120, 148, 255, 1) calc(var(--space) * 5),
+              rgb(216, 117, 255) calc(var(--space) * 6),
+              rgb(255, 119, 115) calc(var(--space) * 7)
+            ),
+            repeating-linear-gradient(
+              133deg,
+              #0e152e 0%,
+              hsl(180, 10%, 60%) 3.8%,
+              hsl(180, 29%, 66%) 4.5%,
+              hsl(180, 10%, 60%) 5.2%,
+              #0e152e 10%,
+              #0e152e 12%
+            ),
+            radial-gradient(
+              farthest-corner circle at ${x}% ${y}%,
+              rgba(0, 0, 0, 0.1) 12%,
+              rgba(0, 0, 0, 0.15) 20%,
+              rgba(0, 0, 0, 0.25) 120%
+            )`
+              ),
+              filter: filter,
+              WebkitFilter: filter,
+            }}
+            className="cardShine"
+          ></a.div>
         </a.div>
       </div>
     </a.div>
